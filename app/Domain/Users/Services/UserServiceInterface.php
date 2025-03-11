93c003cf4a1cd092bc7e41a66f750edb8549ee79
Виноградов Павел <p.vinogradov@realize.dev>
2025-03-11 15:01:54 +0300

    init

---
+++ app/Domain/Users/Services/UserServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Users\Services;
+
+use App\Core\DTO\PaginationDTO;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+interface UserServiceInterface
+{
+    public function paginate(PaginationDTO $paginationDTO): LengthAwarePaginator;
+}
---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
+    MongoDB\Laravel\MongoDBServiceProvider::class,
+    App\Providers\ODataServiceServiceProvider::class,
+];
